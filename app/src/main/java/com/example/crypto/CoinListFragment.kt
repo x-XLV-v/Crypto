0df429751c6b01efa82a19c57d0e44518a12bb29
x-XLV-v <explay_one@mail.ru>
2022-03-02 20:48:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/crypto/CoinListFragment.kt
@@ -0,0 +1,20 @@
+package com.example.crypto
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CoinListFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //Функция вызывается, когда фрагмент прорисовывается впервые. Переменная inflater это объект класса LayoutInflater.
+        //Данный класс из содержимого Layout файла создавать view-элемент. Далее идёт вызов метода inflate. При помощи
+        // метода inflate view-элемент создаётся. В него передаётся идентификатор Layout файла из которого view будет
+        // создаваться. Родительский ViewGroup элемент для создаваемого view, то есть это переменная container. Последний
+        // параметр указывает на то, сделать ли передаваемый в ViewGroup элемент корневым для этого ViewGroup. В данном случае
+        // мы передаём false, так как нам не нужно устанавливать данный фрагмент в качестве root элемента, нам нужны только
+        // некоторые параметры от этого root-элемента.
+        return inflater.inflate(R.layout.fragment_coin_list, container, false)
+    }
+}
